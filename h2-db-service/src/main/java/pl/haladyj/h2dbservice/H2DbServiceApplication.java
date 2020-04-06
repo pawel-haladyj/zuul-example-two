b3b376cd2e714768945ddbcf6c51c7935c3c5987
Pawel Haladyj <pawel@haladyj.pl>
2020-04-06 15:21:48 +0200

    initial commit

---
+++ h2-db-service/src/main/java/pl/haladyj/h2dbservice/H2DbServiceApplication.java
@@ -0,0 +1,13 @@
+package pl.haladyj.h2dbservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class H2DbServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(H2DbServiceApplication.class, args);
+	}
+
+}
---
+++ h2-db-service/src/test/java/pl/haladyj/h2dbservice/H2DbServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.haladyj.h2dbservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class H2DbServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
