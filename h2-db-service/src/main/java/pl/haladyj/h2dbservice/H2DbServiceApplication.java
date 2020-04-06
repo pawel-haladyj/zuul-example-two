68fdf7e58dd44c19654bec7ab4744105ecf0d7f7
Pawel Haladyj <pawel@haladyj.pl>
2020-04-06 18:26:39 +0200

    enabled Eureka Server

--- h2-db-service/src/main/java/pl/haladyj/h2dbservice/H2DbServiceApplication.java
+++ h2-db-service/src/main/java/pl/haladyj/h2dbservice/H2DbServiceApplication.java
@@ -2,8 +2,10 @@ package pl.haladyj.h2dbservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class H2DbServiceApplication {
 
 	public static void main(String[] args) {
